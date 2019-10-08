5214bb4505073d22d26476c37724f1c79c80b2cd
Justin Haubrich <jhaubrichdesign@gmail.com>
2019-10-08 16:21:25 -0500

    Initial Commit, Front end looks good
    Request:
    	basically we need a tool that you can upload a file (template)
    to or enter the template html and it spit out the compiled js function
    	https://mozilla.github.io/nunjucks/

---
+++ ScriptHandler_node.js
@@ -0,0 +1,11 @@
+//Node JS Code that will take the html template as input and output a precompiled js function from nunjucks
+//
+const nunjucks = require('nunjucks');
+const app = express();
+const port = 3033; //ports 2999 - 3032 are reserved for the styleAssembler app
+
+app.post('/templateCompiler/', function (req, res) {
+
+}
+
+
---
+++ templateTool_js.js
@@ -0,0 +1,49 @@
+//Javascript to handle the template tool
+const config = {};
+const script_path = '';
+
+//Using the FileReader API (https://developer.mozilla.org/en-US/docs/Web/API/FileReader)
+var reader = new FileReader()
+	reader.onload = event => {console.log(event.target.result); config.text = event.target.result;} // desired file content
+	reader.onerror = error => reject(error)
+
+//when the submit button is clicked, check if a file was uploaded or if a template was pasted
+$('#submitButton').click(function (e) {
+	console.log('submit buttons clicked', e);
+	//check if a file was uploaded
+	if (document.getElementsByTagName('input')[0].files[0] != undefined) {
+		config.templateType = 'file';
+		var file = document.getElementsByTagName('input')[0].files[0];
+		reader.readAsText(document.getElementsByTagName('input')[0].files[0]); // you could also read images and other binaries
+	}
+	//user pasted a template
+	else if ( $('textarea').val().toString().length > 1) {
+		console.log('A Template was pasted...');
+		config.templateType = 'paste';
+		config.text = $('textarea').val().toString();
+	}
+	//user did not upload or paste a template...
+	else {
+		new swal('Upload or Paste a template...');
+		return;
+	}
+	//going to use ajax to send the template to a node script for nunjucks to precompile it and return a function
+	$.ajax({
+		url: script_path,
+		method: 'POST',
+		data: {template: config.text},
+		success: function (res, status, jqXHR) {
+			console.log(res, status, jqXHR);
+
+		},
+		error: function (jqXHR, status, error) {
+			console.log(jqXHR, status, error);
+		}
+		
+
+	
+	});
+	
+});
+
+
